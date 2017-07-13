1f13590f70c3f2f969235d0b48cd2775a8eb71fc
Carlos Casa <carlos.cupeiro@grupocmc.es>
2017-07-13 21:06:03 +0200

    Primera subida

---
+++ Assets/Scripts/Scene_Controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Controller : MonoBehaviour {
+
+	public void ChangeScene(string nameScene){
+		print ("Change to Scene --> " + nameScene);
+		SceneManager.LoadScene (nameScene);
+	}
+
+	public void Exit(){
+		print ("Exit game"); 
+		Application.Quit ();
+	}
+}
