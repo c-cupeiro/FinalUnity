a82e35fd6791460aca000efdfc712dc8146b589d
Carlos Casa <carlos.cupeiro@grupocmc.es>
2017-07-20 21:22:53 +0200

    Cambios y practica final para el video

--- Assets/Scripts/Enemy_2_Trigger.cs
+++ Assets/Scripts/Enemy_2_Trigger.cs
@@ -16,13 +16,15 @@ public class Enemy_2_Trigger : MonoBehaviour {
 			float offset = 0.4f;
 			//Debug.Log ("Enemy.y --> " + (parent.transform.position.y + offset));
 			//Debug.Log ("Player.y --> " + col.transform.position.y);
-			if (!((parent.transform.position.y+offset) < col.transform.position.y)) {
+			if (!((parent.transform.position.y + offset) < col.transform.position.y)) {
 				Destroy (parent);
 				parent.SendMessage ("StartSmoke");
-			}else {
-				col.SendMessage ("EnemyHitDead");
+			} else {
+				col.transform.parent.SendMessage ("EnemyHitDead");
 			}
 
+		} else if (col.gameObject.tag == "Player") {
+			col.SendMessage ("EnemyHitDead");
 		}
 	}
 }
